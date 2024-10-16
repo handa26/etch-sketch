2b09562673bef6425fade0919bbae57ea96ee85e
Ananda Muhammad <anandamuhammad@proton.me>
2024-10-16 14:14:11 +0800

    Add hover effect

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector(".container");
-
+let boxes;
 
 for (let i = 1; i <= 256; i++) {
   const itemContainer = document.createElement("div");
   const counterDiv = document.createElement("div");
+  boxes = document.querySelectorAll(".box");
 
   itemContainer.classList.add("box");
   counterDiv.innerHTML = i;
@@ -11,3 +12,7 @@ for (let i = 1; i <= 256; i++) {
   container.appendChild(itemContainer);
   itemContainer.appendChild(counterDiv);
 }
+
+boxes.forEach((box) => box.addEventListener("mouseover", (e) => {
+  box.style.backgroundColor = "black"
+}));
