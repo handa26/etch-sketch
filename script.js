867314fee8f0566dc8beac0e271e6471e9b801ab
Ananda Muhammad <anandamuhammad@proton.me>
2024-10-18 10:20:08 +0800

    Dynamically change size grid

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 const container = document.querySelector(".container");
-let boxes;
+const changeGrid = document.querySelector("#change-grid");
 
-for (let i = 1; i <= 256; i++) {
-  const itemContainer = document.createElement("div");
-  const counterDiv = document.createElement("div");
-  boxes = document.querySelectorAll(".box");
+function createGrid(gridSize) {
+  container.innerHTML = "";
 
-  itemContainer.classList.add("box");
-  counterDiv.innerHTML = i;
+  let boxes;
 
-  container.appendChild(itemContainer);
-  itemContainer.appendChild(counterDiv);
+  let containerWidth = 840;
+  container.style.width = `${containerWidth}px`;
+
+  let boxSize = containerWidth / gridSize;
+
+  for (let i = 1; i <= gridSize ** 2; i++) {
+    const itemContainer = document.createElement("div");
+    const counterDiv = document.createElement("div");
+    boxes = document.querySelectorAll(".box");
+
+    itemContainer.classList.add("box");
+
+    itemContainer.style.width = `${boxSize}px`;
+    itemContainer.style.height = `${boxSize}px`;
+
+    container.appendChild(itemContainer);
+    itemContainer.appendChild(counterDiv);
+  }
+
+  boxes.forEach((box) =>
+    box.addEventListener("mouseover", (e) => {
+      box.style.backgroundColor = "black";
+    })
+  );
 }
 
-boxes.forEach((box) => box.addEventListener("mouseover", (e) => {
-  box.style.backgroundColor = "black"
-}));
+function updateGrid() {
+  let userGridSize;
+
+  do {
+    userGridSize = parseInt(
+      prompt("Enter grid size (e.g., 8 for 8x8 grid, limit to 100 only):")
+    );
+  } while (isNaN(userGridSize) || userGridSize <= 0 || userGridSize > 100);
+
+  createGrid(userGridSize);
+}
+
+changeGrid.addEventListener("click", () => {
+  updateGrid();
+});
+
+createGrid(16);
